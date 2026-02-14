918d9f8af6e36bf6941bb0e12d1791ce14e09d8e
ken5z <149224206+ken5z@users.noreply.github.com>
2026-02-15 01:14:21 +0530

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -3,11 +3,9 @@ self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("fetch", () => {});
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("service-worker.js");
-   function unlock() {
-  const pass = document.getElementById("pwd").value;
-  if (pass === "jjk123") {
-    document.getElementById("lock").style.display = "none";
+   
   }
 }
 
     
+
