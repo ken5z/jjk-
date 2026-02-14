4d50754227ee8e581574e41030c96ebfa8b03dc3
ken5z <149224206+ken5z@users.noreply.github.com>
2026-02-15 01:27:47 +0530

    Delete serviceworker.js

--- serviceworker.js
+++
@@ -1,9 +0,0 @@
-sself.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", () => self.clients.claim());
-
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("service-worker.js");
-    
-}
-
-
