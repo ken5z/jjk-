b0bafe0e70c874ddbdd7a4a0815f85a64fa51416
ken5z <149224206+ken5z@users.noreply.github.com>
2026-02-15 01:04:41 +0530

    Add files via upload

---
+++ serviceworker.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", () => self.skipWaiting());
+
+self.addEventListener("fetch", () => {});
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("service-worker.js");
+    function unlock() {
+    const pass = document.getElementById("pwd").value;
+    if (pass === "jjk123") {
+        document.getElementById("lock").remove();
+    }
+}
+
+}
