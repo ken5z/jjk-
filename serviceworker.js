3c4807fe60cc7acc1b3be9aed8b2e4112cdffc24
ken5z <149224206+ken5z@users.noreply.github.com>
2026-02-15 01:19:36 +0530

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,6 +1,6 @@
 self.addEventListener("install", () => self.skipWaiting());
-
-self.addEventListener("fetch", () => {});
+self.addEventListener("activate", () => self.clients.claim());
+s
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("service-worker.js");
    
@@ -8,5 +8,6 @@ if ("serviceWorker" in navigator) {
 }
 
     
+
 
 
